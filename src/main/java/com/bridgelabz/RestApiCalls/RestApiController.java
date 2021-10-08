5ef41044b4491f6f181b4b0c8b665c3eff96c6ff
ashwith31 <ashwitash@gmail.com>
2021-10-08 17:10:14 +0530

    Added: Used GET Request method and passed name as query parameter

--- src/main/java/com/bridgelabz/RestApiCalls/RestApiController.java
+++ src/main/java/com/bridgelabz/RestApiCalls/RestApiController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.RestApiCalls;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class RestApiController {
     @GetMapping(value = "/hello")
     public String simple(){
         return "Hello From BridgeLabz";
+    }
+
+    //http://localhost:8080/hello/query?name=Mark
+    @GetMapping(value = "/hello/query")
+    public String simple1(@RequestParam String name){
+        return "Hello "+name+" from Bridgelabz";
     }
 }
